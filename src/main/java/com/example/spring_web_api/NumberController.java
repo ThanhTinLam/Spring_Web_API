ecf180ec67103f13cb4d706974fa3d296b62b1a8
ThanhTinLam <lamtin290592@gmail.com>
2022-12-23 22:08:08 -0600

    sum prime number in a list Calculate the average of a series of numbers sort in ascending order of numbers test on Postman

--- src/main/java/com/example/spring_web_api/NumberController.java
+++ src/main/java/com/example/spring_web_api/NumberController.java
@@ -16,22 +16,49 @@ public class NumberController {
     @PostMapping("/max")
     public Integer Number(@RequestBody MaxMin maxMin) {
         MaxMinAlgo x = new MaxMinAlgo();
-        int max = x.findMax(maxMin) ;
+        int max = x.findMax(maxMin);
         return max;
     }
+
     @PostMapping("/sum")
-    public Integer Sum(@RequestBody ThreeNumber sum3){
+    public Integer Sum(@RequestBody ThreeNumber sum3) {
         SumAlgo x = new SumAlgo();
         int sum = x.sum3Number(sum3);
         return sum;
     }
+
     @PostMapping("/sumarray")
-    public Integer sumArray(@RequestBody SumListArray sumListArray){
+    public Integer sumArray(@RequestBody SumListArray sumListArray) {
         SumAlgo x = new SumAlgo();
         int sum = x.sumListArray(sumListArray);
         return sum;
     }
 
+    @PostMapping("/sumevennumber")
+    public Integer sumEvenNumber(@RequestBody SumListArray sumEvenNumber) {
+        SumAlgo x = new SumAlgo();
+        int sum = x.sumEvenNumber(sumEvenNumber);
+        return sum;
+    }
+
+    @PostMapping("/sumprimenumber")
+    public Integer sumPrimeNumber(@RequestBody SumListArray sumPrimeNumber){
+        SumAlgo x = new SumAlgo();
+        int sum = x.sumPrimeNumber(sumPrimeNumber);
+        return sum;
+    }
+    @PostMapping("/average")
+    public Double averageNumber(@RequestBody SumListArray averageNumber){
+        SumAlgo x = new SumAlgo();
+        double avg = x.averageNumber(averageNumber);
+        return avg;
+    }
+    @PostMapping("/acsendingnumber")
+    public SumListArray acsendingNumber(@RequestBody SumListArray ascendingNumber){
+        SumAlgo x = new SumAlgo();
+        x.ascendingNumber(ascendingNumber);
+        return ascendingNumber;
+    }
 }
 
 
